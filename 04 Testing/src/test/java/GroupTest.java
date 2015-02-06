9e07fde003eabb89a5f8d1e6f278d7d22d27fbff
Nicole <NicoleLaptop@gmx.de>
2015-02-06 23:19:38 +0100

    Umlaute geaendert

--- 04 Testing/src/test/java/GroupTest.java
+++ 04 Testing/src/test/java/GroupTest.java
@@ -4,43 +4,43 @@ import org.kramerlab.atmocalc.objects.*;
 
 public class GroupTest {
 
-	// Der gleiche Nutzer darf nicht einer Gruppe hinzugefŸgt werden kšnnen, in der er schon Member ist
-	@org.junit.Test (expected = IllegalArgumentException.class)
-	public void testGroup1() throws Exception {
-		User user = new User();
-		Group group = new Group();
-		group.addMember(user);
-		group.addMember(user);
-	}
-	
-	// Ein Nutzer, der nicht Member einer Gruppe ist, darf nicht aus dem Memberset gelšscht werden kšnnen (existiert nicht)
-	@org.junit.Test (expected = IllegalArgumentException.class)
-	public void testGroup2() throws Exception {
-		User user = new User();
-		(new Group()).removeMember(user);
-	}
-	
-	// Das null-Objekt darf nicht dem Memberset hinzugefŸgt werden kšnnen
-	@org.junit.Test (expected = IllegalArgumentException.class)
-	public void testGroup4() throws Exception {
-		(new Group()).addMember(null);
-	}
-	
-	// Das null-Objekt darf nicht aus dem Memberset gelšscht werden kšnnen
-	@org.junit.Test (expected = IllegalArgumentException.class)
-	public void testGroup5() throws Exception {
-		(new Group()).removeMember(null);
-	}
-	
-	@org.junit.Test
-	public void testGroup6() throws Exception {
-		User user1 = new User();
-		User user2 = new User();
-		User user3 = new User();
-		Group group = new Group();
-		group.addMember(user1);
-		group.addMember(user2);
-		group.removeMember(user1);
-		group.addMember(user3);
-	}
+  // Der gleiche Nutzer darf nicht einer Gruppe hinzugefuegt werden koennen, in der er schon Member ist
+  @org.junit.Test (expected = IllegalArgumentException.class)
+  public void testGroup1() throws Exception {
+    User user = new User();
+    Group group = new Group();
+    group.addMember(user);
+    group.addMember(user);
+  }
+  
+  // Ein Nutzer, der nicht Member einer Gruppe ist, darf nicht aus dem Memberset geloescht werden koennen (existiert nicht)
+  @org.junit.Test (expected = IllegalArgumentException.class)
+  public void testGroup2() throws Exception {
+    User user = new User();
+    (new Group()).removeMember(user);
+  }
+  
+  // Das null-Objekt darf nicht dem Memberset hinzugefügt werden kšnnen
+  @org.junit.Test (expected = IllegalArgumentException.class)
+  public void testGroup4() throws Exception {
+    (new Group()).addMember(null);
+  }
+  
+  // Das null-Objekt darf nicht aus dem Memberset geloescht werden kšnnen
+  @org.junit.Test (expected = IllegalArgumentException.class)
+  public void testGroup5() throws Exception {
+    (new Group()).removeMember(null);
+  }
+  
+  @org.junit.Test
+  public void testGroup6() throws Exception {
+    User user1 = new User();
+    User user2 = new User();
+    User user3 = new User();
+    Group group = new Group();
+    group.addMember(user1);
+    group.addMember(user2);
+    group.removeMember(user1);
+    group.addMember(user3);
+  }
 }
