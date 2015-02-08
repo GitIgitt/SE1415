387867ada2dbce3ba1cd9d3b16205cec3532ce30
Lars Porth <lporth@students.uni-mainz.de>
2015-02-09 00:52:14 +0100

    Update: 	manageraccseeor: 		- GetUserbyNameTest

--- 04 Testing/src/test/java/manageraccessor/GetUserbyNameTest.java
+++ 04 Testing/src/test/java/manageraccessor/GetUserbyNameTest.java
@@ -7,29 +7,49 @@ import org.junit.Before;
 import org.kramerlab.atmocalc.database.accessors.ManagerAccessor;
 import org.kramerlab.atmocalc.objects.User;
 
-public class GetUserbyNameTest{
-  ManagerAccessor ac;
-  User user1,user2,user3,user4;
-  @Before
-  public void setUp(){
-    ac = new ManagerAccessor();
-    user1 = ac.newUser("test1", "test1@email.de", "test");
-    user2 = ac.newUser("test2", "test2@email.de", "test");
-    user3 = ac.newUser("test3", "test3@email.de", "test");
-  }
-  @After
-  public void tearDown(){
-    //Lösche die Nutzer
-    user1.delete();
-    user2.delete();
-    user3.delete();
-    user4.delete();
-  }
-  @org.junit.Test
-  public void testGetUserbyName1(){
-    assertTrue((user1==ac.getUserByName("test1")));
-    assertTrue((user2==ac.getUserByName("test2")));
-    assertTrue((user3==ac.getUserByName("test3")));
-  }
-  
+public class GetUserbyNameTest {
+	ManagerAccessor ac;
+	User user1, user2, user3;
+
+	@Before
+	public void setUp() {
+		ac = new ManagerAccessor();
+		user1 = ac.newUser("test1", "test1@email.de", "test");
+		user2 = ac.newUser("test2", "test2@email.de", "test");
+		user3 = ac.newUser("test3", "test3@email.de", "test");
+	}
+
+	@After
+	public void tearDown() {
+		// Lösche die Nutzer
+		user1.delete();
+		user2.delete();
+		user3.delete();
+	}
+
+	@org.junit.Test
+	public void testGetUserbyName1() {
+		assertTrue((user1 == ac.getUserByName("test1")));
+		assertTrue((user2 == ac.getUserByName("test2")));
+		assertTrue((user3 == ac.getUserByName("test3")));
+	}
+
+	@org.junit.Test
+	public void testGetUserbyName2() {
+		assertFalse((user1 == ac.getUserByName("test2")));
+		assertFalse((user1 == ac.getUserByName("test3")));
+		assertFalse((user2 == ac.getUserByName("test1")));
+		assertFalse((user2 == ac.getUserByName("test3")));
+		assertFalse((user3 == ac.getUserByName("test1")));
+		assertFalse((user3 == ac.getUserByName("test2")));
+	}
+
+	@org.junit.Test
+	public void testGetUserbyName3() {
+		user1.setName("test1_1");
+		assertFalse((user1 == ac.getUserByName("test1")));
+		assertTrue((user1 == ac.getUserByName("test1_1")));
+		assertTrue((user2 == ac.getUserByName("test2")));
+		assertTrue((user3 == ac.getUserByName("test3")));
+	}
 }
