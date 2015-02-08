31fe053c9e5558f28031f2319e4a4d3c4a3f27a0
Lars Porth <lporth@students.uni-mainz.de>
2015-02-08 21:47:40 +0100

    GetUri Test bei User

--- 04 Testing/src/test/java/atmocalcuser/GetUriTest.java
+++ 04 Testing/src/test/java/atmocalcuser/GetUriTest.java
@@ -9,14 +9,12 @@ import org.junit.Before;
 import org.kramerlab.atmocalc.manager.AtmoCalcManager;
 import org.kramerlab.atmocalc.objects.*;
 
-public class GetNameTest {
+public class GetUriTest {
 	final char[] zeichen = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789".toCharArray();
-	ArrayList<String> vergleich;
 	ArrayList<User> testuser;
 	AtmoCalcManager ma = new AtmoCalcManager(null);
 	@Before
 	public void setUp(){
-		vergleich = new ArrayList<String>();
 		testuser = new ArrayList<User>();
 		char[] testname;
 		for (int i = 0; i < 100; i++){
@@ -25,7 +23,6 @@ public class GetNameTest {
 				testname[j] = zeichen[(int)(Math.random()*62)]; 
 			}
 			String s = new String(testname);
-			vergleich.add(s);
 			testuser.add(ma.newUser(s, s+"@test.de", "test"));
 		}
 	}
@@ -41,7 +38,7 @@ public class GetNameTest {
 	@org.junit.Test
 	public void testGetUser() throws Exception {
 		for (int i = 0; i < testuser.size(); i++){
-			assertTrue((vergleich.get(i)).toLowerCase() == testuser.get(i).getName().toLowerCase());
+			assertTrue((testuser.get(i).databaseId) == testuser.get(i).getUri());
 		}
 			
 	}
