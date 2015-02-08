2c1debece7bbcb4e66052b7e8d257d51450c00a8
Nicole <NicoleLaptop@gmx.de>
2015-02-08 22:50:52 +0100

    before und after in datasetTest eingefuegt

--- 04 Testing/src/test/java/atmocalcdataset/DatasetTest.java
+++ 04 Testing/src/test/java/atmocalcdataset/DatasetTest.java
@@ -4,13 +4,26 @@ import static org.junit.Assert.*;
 
 import org.kramerlab.atmocalc.objects.*;
 
+import org.junit.After;
+import org.junit.Before;
 
 public class DatasetTest {
-
+	Dataset d1, d2;
+	@Before
+	public void setUp(){
+		Dataset d1 = new Dataset();
+	}
+	
+	@After
+	public void tearDown(){
+		d1.delete();
+		d2.delete();
+	}
+	
+	
 	// Copy muss das Dataset korrekt kopieren
 	@org.junit.Test
 	public void testDataset1() throws Exception {
-		Dataset d1 = new Dataset();
 		Dataset d2 = d1.copy();
 		assertEquals("Copy korrekt", (d1.getRawData() == d2.getRawData() && d1.getColumnNames() == d2.getColumnNames() && d1.getName() == d2.getName() && d1.getUri() == d2.getUri()));
 	}
@@ -18,7 +31,6 @@ public class DatasetTest {
 	// colCount entspricht der Anzahl der Namen in columnNames
 	@org.junit.Test
 	public void testDataset2() throws Exception {
-		Dataset d1 = new Dataset();
 		assertEquals("colCount korrekt", d1.colCount() == d1.getColumnNames().size());
 	}
 }
